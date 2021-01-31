581f9e830c8c464e7209ab85d95747b0ee0f1803
user <nix_olympica@tut.by>
2021-01-31 18:32:38 +0200

    second commit

---
+++ src/Start2021.java
@@ -0,0 +1,91 @@
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Start2021 {
+
+    public static void main(String[] args) throws Exception {
+
+    	Start2021 obj = new Start2021();
+        System.out.println("Testing - Send Http POST request");
+
+        for(int i = 0; i < 60; i++) {
+        	obj.sendPost();
+        	Thread.sleep(1000);
+        }
+    }
+
+    private void sendGet() throws Exception {
+
+    	String url = "http://localhost:8081/Point_one(Web)17";
+
+        HttpURLConnection httpClient =
+                (HttpURLConnection) new URL(url).openConnection();
+
+        // optional default is GET
+        httpClient.setRequestMethod("GET");
+
+        //add request header
+        httpClient.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        int responseCode = httpClient.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        //..OUTPUT
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(httpClient.getInputStream()))) {
+
+            StringBuilder response = new StringBuilder();
+            String line;
+
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+
+            //print result
+            System.out.println(response.toString());
+
+        }
+    }
+
+    private void sendPost() throws Exception {
+
+        String url = "http://localhost:8080/WebApp/MyServlet?user=123";
+
+        HttpURLConnection httpClient = (HttpURLConnection) new URL(url).openConnection();
+
+        //add reuqest header
+        httpClient.setRequestMethod("POST");
+        httpClient.setRequestProperty("User-Agent", "Mozilla/5.0");
+        httpClient.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+
+        int responseCode = httpClient.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(httpClient.getInputStream()))) {
+
+            String line;
+            StringBuilder response = new StringBuilder();
+
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+
+            //print result
+            System.out.println(response.toString());
+
+        }
+
+    }
+
+}
